e16545bce476256e9fb8e9d330007cc774298682
nagatax <25280585+nagatax@users.noreply.github.com>
2026-08-22 20:09:44 +0900

    RustのLSP設定と診断表示を改善

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -34,7 +34,16 @@ return {
       local capabilities = require("blink.cmp").get_lsp_capabilities()
 
       vim.lsp.config("clangd", { capabilities = capabilities })
-      vim.lsp.config("rust_analyzer", { capabilities = capabilities })
+      vim.lsp.config("rust_analyzer", {
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            check = {
+              command = "clippy",
+            },
+          },
+        }
+      })
       vim.lsp.config("gopls", { capabilities = capabilities })
       vim.lsp.config("terraformls", { capabilities = capabilities, filetypes = { "tf", "terraform", "terraform-vars" } })
 
@@ -42,6 +51,31 @@ return {
       vim.lsp.enable("rust_analyzer")
       vim.lsp.enable("gopls")
       vim.lsp.enable("terraformls")
+
+      -- 診断表示
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        severity_sort = true,
+        update_in_insert = false,
+        float = {
+          border = "rounded",
+          source = true,
+        },
+      })
+
+      -- Rustファイル保存時にrustfmt
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.rs",
+        callback = function(args)
+          vim.lsp.buf.format({
+            bufnr = args.buf,
+            async = false,
+            timeout_ms = 3000,
+          })
+        end,
+      })
     end,
   },
 }
